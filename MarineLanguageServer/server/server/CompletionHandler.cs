5de02cac0ef9134fecfaf9cf63689de6b4d5271c
hiroki199990625 <hiroki19990625@gmail.com>
2021-01-12 00:34:03 +0900

    feat 関数とパラメータの補完

--- MarineLanguageServer/server/server/CompletionHandler.cs
+++ MarineLanguageServer/server/server/CompletionHandler.cs
@@ -1,7 +1,11 @@
-﻿using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+﻿using MarineLang.LexicalAnalysis;
+using MarineLang.Models.Asts;
+using MarineLang.SyntaxAnalysis;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,10 +21,29 @@ namespace server
 
         public async Task<CompletionList> Handle(CompletionParams request, CancellationToken cancellationToken)
         {
-            var keywords = Task.Run(() => CreateKeywords());
-            var snippets = Task.Run(() => CreateSnippets());
+            var text = File.ReadAllText(request.TextDocument.Uri.Path.Remove(0, 1));
+            var tokens = new LexicalAnalyzer().GetTokens(text);
+            var result = new SyntaxAnalyzer().Parse(tokens);
 
-            return new CompletionList((await Task.WhenAll(keywords, snippets)).SelectMany(e => e));
+            var tasks = new List<Task<IEnumerable<CompletionItem>>>();
+            FuncDefinitionAst currentFuncDefAst = null;
+            if (!result.IsError)
+            {
+                currentFuncDefAst = await Task.Run(() => GetCurrentFunctionAst(result.Value, request.Position));
+
+                if (currentFuncDefAst != null)
+                {
+                    tasks.Add(Task.Run(() => CreateFunctions(result.Value)));
+
+                    tasks.Add(Task.Run(() => CreateFunctionParamaters(currentFuncDefAst)));
+
+                    tasks.Add(Task.Run(() => CreateKeywords()));
+                }
+            }
+
+            tasks.Add(Task.Run(() => CreateSnippets(currentFuncDefAst)));
+
+            return new CompletionList((await Task.WhenAll(tasks)).SelectMany(e => e));
         }
 
         public void SetCapability(CompletionCapability capability)
@@ -37,28 +60,44 @@ namespace server
             };
         }
 
-        private IEnumerable<CompletionItem> CreateSnippets()
+        private IEnumerable<CompletionItem> CreateSnippets(FuncDefinitionAst ast)
         {
-            return new[]
+            List<CompletionItem> items = new List<CompletionItem>();
+            if (ast != null)
             {
-                CreateSnippetItem("fun", "fun ${1:foo}()\n\t$2\nend"),
-                CreateSnippetItem("fmain", "fun main()\n\t$2\nend"),
-                CreateSnippetItem("letc", "let ${1:foo} = $2"),
-                CreateSnippetItem("if", "if ($1) {\n\t$2\n}"),
-                CreateSnippetItem("else", "else {\n\t$1\n}"),
-                CreateSnippetItem("elseif", "else if ($1) {\n\t$2\n}"),
-                CreateSnippetItem("while", "while ($1) {\n\t$2\n}"),
-                CreateSnippetItem("for", "for ${1:i} = $2, $3, $4 {\n\t$5\n}"),
-            };
+                items.Add(CreateSnippetItem("letc", "let ${1:foo} = $2"));
+                items.Add(CreateSnippetItem("if", "if ($1) {\n\t$2\n}"));
+                items.Add(CreateSnippetItem("else", "else {\n\t$1\n}"));
+                items.Add(CreateSnippetItem("elseif", "else if ($1) {\n\t$2\n}"));
+                items.Add(CreateSnippetItem("while", "while ($1) {\n\t$2\n}"));
+                items.Add(CreateSnippetItem("for", "for ${1:i} = $2, $3, $4 {\n\t$5\n}"));
+            }
+            else
+            {
+                items.Add(CreateSnippetItem("fun", "fun ${1:foo}()\n\t$2\nend"));
+                items.Add(CreateSnippetItem("fmain", "fun main()\n\t$2\nend"));
+            }
+
+            return items;
         }
 
-        private CompletionItem CreateCompletionItem(string label, CompletionItemKind kind, StringOrMarkupContent document = null)
+        private IEnumerable<CompletionItem> CreateFunctions(ProgramAst ast)
+        {
+            return ast.funcDefinitionAsts.Select(e => CreateCompletionItem(e.funcName, CompletionItemKind.Function, $"function {e.funcName}({string.Join(", ", e.args.Select(a => a.VarName))})"));
+        }
+
+        private IEnumerable<CompletionItem> CreateFunctionParamaters(FuncDefinitionAst ast)
+        {
+            return ast.args.Select(e => CreateCompletionItem(e.VarName, CompletionItemKind.Field, $"paramater {e.VarName}"));
+        }
+
+        private CompletionItem CreateCompletionItem(string label, CompletionItemKind kind, string detail = null)
         {
             return new CompletionItem()
             {
                 Label = label,
                 Kind = kind,
-                Documentation = document
+                Detail = detail
             };
         }
 
@@ -72,6 +111,25 @@ namespace server
                 InsertTextFormat = InsertTextFormat.Snippet,
                 InsertTextMode = InsertTextMode.AdjustIndentation
             };
+        }
+
+        private FuncDefinitionAst GetCurrentFunctionAst(ProgramAst programAst, Position position)
+        {
+            foreach (FuncDefinitionAst ast in programAst.funcDefinitionAsts)
+            {
+                if (Contains(ast, position))
+                {
+                    return ast;
+                }
+            }
+
+            return null;
+        }
+
+        private bool Contains(IAst ast, Position position)
+        {
+            return ast.Start.line <= position.Line + 1
+                && ast.End.line >= position.Line + 1;
         }
     }
 }
