bf2b8f18ab12069d94fe0c8c73d8b0dc5bed96fa
hiroki199990625 <hiroki19990625@gmail.com>
2022-09-04 18:44:00 +0900

    候補選出が無限ループする時があるので、タイムアウトを設定

--- MarineLanguageServer/server/server/Handlers/CompletionHandler.cs
+++ MarineLanguageServer/server/server/Handlers/CompletionHandler.cs
@@ -8,11 +8,14 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
 using OmniSharp.Extensions.LanguageServer.Protocol.Document;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Window;
 
 namespace MarineLang.LanguageServerImpl.Handlers
 {
     public class CompletionHandler : ICompletionHandler
     {
+        private const int CompletionTimeout = 5000;
+
         private readonly ILanguageServerFacade _languageServerFacade;
         private readonly WorkspaceService _workspaceService;
         private readonly CompletionService _completionService;
@@ -36,21 +39,38 @@ namespace MarineLang.LanguageServerImpl.Handlers
             };
         }
 
-        public Task<CompletionList> Handle(CompletionParams request, CancellationToken cancellationToken)
+        public async Task<CompletionList> Handle(CompletionParams request, CancellationToken cancellationToken)
         {
-            var text = _workspaceService.GetMarineFileBuffer(request.TextDocument.Uri.Path);
-            var tokens = new LexicalAnalyzer().GetTokens(text);
-            var result = new SyntaxAnalyzer().Parse(tokens);
-
-            var list = new CompletionList();
-            if (result.programAst != null)
+            var items = Task.Run(() =>
             {
-                list = new CompletionList(_completionService.GetCompletions(result.programAst,
-                    request.TextDocument.Uri.Path, request.Context?.TriggerCharacter, request.Context?.TriggerKind ?? 0,
-                    request.Position));
+                var text = _workspaceService.GetMarineFileBuffer(request.TextDocument.Uri.Path);
+                var tokens = new LexicalAnalyzer().GetTokens(text);
+                var result = new SyntaxAnalyzer().Parse(tokens);
+
+                var list = new CompletionList();
+                if (result.programAst != null)
+                {
+                    return new CompletionList(_completionService.GetCompletions(result.programAst,
+                        request.TextDocument.Uri.Path, request.Context?.TriggerCharacter,
+                        request.Context?.TriggerKind ?? 0,
+                        request.Position));
+                }
+
+                return list;
+            }, cancellationToken);
+            var cancelTask = Task.Run(async () =>
+            {
+                await Task.Delay(CompletionTimeout, cancellationToken);
+                return new CompletionList();
+            }, cancellationToken);
+
+            var task = await Task.WhenAny(items, cancelTask);
+            if (task.Status != TaskStatus.Canceled && task != items)
+            {
+                _languageServerFacade.Window.ShowError("Candidate selection timed out.");
             }
 
-            return Task.FromResult(list);
+            return await task;
         }
 
         /*private IEnumerable<CompletionItem> CreateVariables(FuncDefinitionAst ast, Position position)
